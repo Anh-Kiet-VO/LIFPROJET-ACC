9807ba7514180100b65d516c8d0113cd280c509a
Anh-Kiet <anhkietakpro@gmail.com>
2022-03-27 14:44:36 +0200

    Ajout id

--- client/src/components/CompSheet.js
+++ client/src/components/CompSheet.js
@@ -4,8 +4,8 @@ export default function CompSheet(props) {
   console.log(props.genres);
 
   return (
-    <div key={props.id} className="sheet">
-        <img src={props.url_img} alt={props.title}/>
+    <div key={props.id} id={props.id} className="sheet">
+        <img src={props.url} alt={props.title}/>
         <div className="card-text">
         <h2>{props.title}</h2>
         <p>{props.date}</p>
