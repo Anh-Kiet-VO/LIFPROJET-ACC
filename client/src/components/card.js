e144dd84744109bd7378b906a1599c3e684ee6bc
Anh-Kiet <anhkietakpro@gmail.com>
2022-03-27 14:44:15 +0200

    <Link to..> sur image

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Card(props) {
   return (
 	<div key={props.id} id={props.id} className='movie-link' onClick={props.getMovieInfo}>
-		<img src={props.url} alt={props.title}/>
+		<Link to={`/detail/${props.id}`}><img src={props.url} alt={props.title}/></Link>
 		<div className='card-text'>
 		<h2>{props.title}</h2>
 		<p>{props.score}</p>
