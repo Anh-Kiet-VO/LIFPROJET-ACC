0079c2b12527eeb1c8c8a79634b1e05d6124182e
Anh-Kiet <anhkietakpro@gmail.com>
2022-03-28 09:54:13 +0200

    indentation

--- client/src/App.js
+++ client/src/App.js
@@ -12,129 +12,130 @@ import Home from "./pages/Home"
 import Navbar from "./components/Navbar";
 
 function App() {
-  /*const [emailReg, setEmailReg] = useState('');
-  const [passwordReg, setPasswordReg] = useState('');
+	/*const [emailReg, setEmailReg] = useState('');
+	const [passwordReg, setPasswordReg] = useState('');
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  const [loginStatus, setLoginStatus] = useState(false);
+	const [loginStatus, setLoginStatus] = useState(false);
 
-  const [isActive, setActive] = useState("false");
+	const [isActive, setActive] = useState("false");
 
-  const displayRegister = () => {
-    setActive(!isActive);
-  };
+	const displayRegister = () => {
+		setActive(!isActive);
+	};
 
-  Axios.defaults.withCredentials = true;
+	Axios.defaults.withCredentials = true;
 
-  const register = () => {
-    Axios.post('http://localhost:3001/register', {
-      email: emailReg,
-      password: passwordReg,
-    }).then((response) => {
-      console.log(response)
-    });
-  };
+	const register = () => {
+		Axios.post('http://localhost:3001/register', {
+		email: emailReg,
+		password: passwordReg,
+		}).then((response) => {
+		console.log(response)
+		});
+	};
 
-  const login = () => {
-    Axios.post('http://localhost:3001/login', {
-      email: email,
-      password: password,
-    }).then((response) => {
-      if(!response.data.auth) {
-        
-        setLoginStatus(false);
-      } else {
-        localStorage.setItem("token", response.data.token);
-        setLoginStatus(true);
-      }
-    });
-  };
+	const login = () => {
+		Axios.post('http://localhost:3001/login', {
+		email: email,
+		password: password,
+		}).then((response) => {
+		if(!response.data.auth) {
+			
+			setLoginStatus(false);
+		} else {
+			localStorage.setItem("token", response.data.token);
+			setLoginStatus(true);
+		}
+		});
+	};
 
-  const userAuthenticated = () => {
-    Axios.get("http://localhost:3001/isUserAuth", {
-      headers: {
-        "x-access-token": localStorage.getItem("token"),
-      },
-    }).then((response) => {
-      console.log(response);
-    })
-  }*/
-  return (
-    <div className="App">
-      
-      <BrowserRouter>
-      <Navbar />
-        <Routes>
+	const userAuthenticated = () => {
+		Axios.get("http://localhost:3001/isUserAuth", {
+		headers: {
+			"x-access-token": localStorage.getItem("token"),
+		},
+		}).then((response) => {
+		console.log(response);
+		})
+	}*/
 
-          <Route path="/detail/:id" element={<Moviedetails/>}></Route>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/movie" element={<Movie/>}></Route>
-        </Routes>
-      </BrowserRouter>
+	return (
+		<div className="App">
 
-      {/* <div className="registration">
-      <div className={isActive? "hidden" : ""} id="register-modal">
-          <h1>Registration</h1>
-          <label>Email</label>
-          <input
-            type="email"
-            onChange={(e) => {
-              setEmailReg(e.target.value);
-            }}
-          />
-          <label>Password</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setPasswordReg(e.target.value);
-            }}
-          />
-          <button onClick={register}>Register</button>
-        </div>
-        
-        <button onClick={displayRegister}>S'inscrire</button>
-      </div> */}
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
 
-      { /*<Register
-        setEmailReg = {setEmailReg}
-        setPasswordReg = {setPasswordReg}
-        register = {register}
-      />
+					<Route path="/detail/:id" element={<Moviedetails />}></Route>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/movie" element={<Movie />}></Route>
+				</Routes>
+			</BrowserRouter>
 
-      <Login
-        setEmail = {setEmail}
-        setPassword = {setPassword}
-        login = {login}
-        loginStatus = {loginStatus}
-        userAuthenticated = {userAuthenticated}
-    />*/ } 
+			{/* <div className="registration">
+		<div className={isActive? "hidden" : ""} id="register-modal">
+			<h1>Registration</h1>
+			<label>Email</label>
+			<input
+				type="email"
+				onChange={(e) => {
+				setEmailReg(e.target.value);
+				}}
+			/>
+			<label>Password</label>
+			<input
+				type="text"
+				onChange={(e) => {
+				setPasswordReg(e.target.value);
+				}}
+			/>
+			<button onClick={register}>Register</button>
+			</div>
+			
+			<button onClick={displayRegister}>S'inscrire</button>
+		</div> */}
 
-      {/*<div className="Login">
-        <h1>Login</h1>
-        <input
-          type="email"
-          placeholder="Email@Email.fr"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder= "Password..."
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button onClick={login}>Login</button>
-      </div>
+			{ /*<Register
+			setEmailReg = {setEmailReg}
+			setPasswordReg = {setPasswordReg}
+			register = {register}
+		/>
 
-      {loginStatus && (
-        <button onClick={userAuthenticated}>Check if authenticate</button>
-      )}}*/}
-    </div>
-  );
+		<Login
+			setEmail = {setEmail}
+			setPassword = {setPassword}
+			login = {login}
+			loginStatus = {loginStatus}
+			userAuthenticated = {userAuthenticated}
+		/>*/ }
+
+			{/*<div className="Login">
+			<h1>Login</h1>
+			<input
+			type="email"
+			placeholder="Email@Email.fr"
+			onChange={(e) => {
+				setEmail(e.target.value);
+			}}
+			/>
+			<input
+			type="password"
+			placeholder= "Password..."
+			onChange={(e) => {
+				setPassword(e.target.value);
+			}}
+			/>
+			<button onClick={login}>Login</button>
+		</div>
+
+		{loginStatus && (
+			<button onClick={userAuthenticated}>Check if authenticate</button>
+		)}}*/}
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/card.js
+++ client/src/components/card.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 export default function Card(props) {
-  return (
-	<div key={props.id} id={props.id} className='movie-link' onClick={props.getMovieInfo}>
-		<Link to={`/detail/${props.id}`}><img src={props.url} alt={props.title}/></Link>
-		<div className='card-text'>
-		<h2>{props.title}</h2>
-		<p>{props.score}</p>
+	return (
+		<div key={props.id} id={props.id} className='movie-link' onClick={props.getMovieInfo}>
+			<Link to={`/detail/${props.id}`}><img src={props.url} alt={props.title} /></Link>
+			<div className='card-text'>
+				<h2>{props.title}</h2>
+				<p>{props.score}</p>
+			</div>
 		</div>
-	</div>
-  )
+	)
 }
