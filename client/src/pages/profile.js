e20210412b6f60492655f7d2a75eec5e0a081895
Anh-Kiet-VO <anhkietakpro@gmail.com>
2022-04-12 15:35:47 +0200

    liste de film dans un bouton

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useEffectLayout } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
@@ -14,26 +14,24 @@ function Profile() {
 				setUsername(response.data[0].username);
 			});
 
+
+	}, [])
+
+	const showList = () => {
 		axios.get(`http://localhost:3001/movieList/${username}`)
 			.then((response) => {
 				setCrudList(response.data) // Erreur fetch, parfois renvoie error
 				// GET http://localhost:3001/movieList/ 404 (Not Found)
 				// Uncaught (in promise) Error: Request failed with status code 404
 			});
-	}, [])
+	}
 
 	return (
 		<div>
 			<h1>Bienvenue {username}</h1>
 
-
+			<button onClick={showList}>show</button>
 			{
-				// crudList.map((val, key) => {
-				// 	return <div>{console.log(val) + val.movieId + val.status + val.score + val.progress + val.userId}</div>
-				// })
-
-				// console.log(crudList.filter(user => user.userId == username).map(val => val.movieId))
-
 				crudList.map((val, key) => {
 					return (
 						<div key={key} className="crud-list">
